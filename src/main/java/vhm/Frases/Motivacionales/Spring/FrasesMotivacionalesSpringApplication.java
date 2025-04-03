53095c44b0efa76f033e47dc0bde311b07c0d106
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-04-03 14:00:23 -0600

    first commit

---
+++ src/main/java/vhm/Frases/Motivacionales/Spring/FrasesMotivacionalesSpringApplication.java
@@ -0,0 +1,13 @@
+package vhm.Frases.Motivacionales.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesMotivacionalesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesMotivacionalesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vhm/Frases/Motivacionales/Spring/FrasesMotivacionalesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package vhm.Frases.Motivacionales.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesMotivacionalesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
